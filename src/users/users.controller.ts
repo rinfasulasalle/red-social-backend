c8b64f5b82c77ef1cd47019a42d5b71f30335574
rinfasulasalle <rinfas@ulasalle.edu.pe>
2024-05-09 23:15:07 -0500

    Validaciones con HTTP Exception

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -18,7 +18,7 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  createUser(@Body() newUser: CreateUserDto): Promise<User> {
+  createUser(@Body() newUser: CreateUserDto) {
     return this.usersService.createUser(newUser);
   }
 
@@ -28,7 +28,7 @@ export class UsersController {
   }
 
   @Get(':id')
-  getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
+  getUser(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.getUser(id);
   }
 
