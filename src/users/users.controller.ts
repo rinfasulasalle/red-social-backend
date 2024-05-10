dae1692aa74013852f8021c515d70ab5916e2cf5
rinfasulasalle <rinfas@ulasalle.edu.pe>
2024-05-09 22:05:36 -0500

    CRUD completo basico funcional API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,21 +2,46 @@ import {
   Controller,
   Get,
   Post,
-  Put,
   Delete,
   Patch,
   Body,
   Param,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  createUser(@Body() newUser: CreateUserDto) {
+  createUser(@Body() newUser: CreateUserDto): Promise<User> {
     return this.usersService.createUser(newUser);
+  }
+
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.usersService.getUsers();
+  }
+
+  @Get(':id')
+  getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
+    return this.usersService.getUser(id);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.deleteUser(id);
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() user: UpdateUserDto,
+  ) {
+    return this.usersService.updateUser(id, user);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -13,5 +14,23 @@ export class UsersService {
   createUser(user: CreateUserDto) {
     const newUser = this.userRepository.create(user);
     return this.userRepository.save(newUser);
+  }
+
+  getUsers() {
+    return this.userRepository.find();
+  }
+
+  getUser(id: number) {
+    return this.userRepository.findOne({
+      where: { id },
+    });
+  }
+
+  deleteUser(id: number) {
+    return this.userRepository.delete({ id });
+  }
+
+  updateUser(id: number, user: UpdateUserDto) {
+    return this.userRepository.update({ id }, user);
   }
 }
