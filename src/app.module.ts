5992b09fcb9e6f0dfb1b5d95562127460df430b9
rinfasulasalle <rinfas@ulasalle.edu.pe>
2024-05-09 20:36:27 -0500

    Se crea el modelo entidad de user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '171222',
+      database: 'db_red_social',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
