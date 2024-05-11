1443b2de43ef9e271d2d072c5598a52709bf971e
rinfasulasalle <rinfas@ulasalle.edu.pe>
2024-05-11 10:50:54 -0500

    hasheamos la password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
 
+import * as bcryptjs from 'bcrypt';
+
 @Injectable()
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
@@ -12,6 +14,8 @@ export class AuthService {
     if (userFound) {
       return new HttpException('User already exists', HttpStatus.CONFLICT);
     }
+    // Se encripta la contraseña
+    registerDto.password = await bcryptjs.hash(registerDto.password, 10);
     const userRegistered = await this.usersService.createUser(registerDto);
     return userRegistered;
   }
